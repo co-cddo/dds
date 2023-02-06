7e0e90b68f1070649e030ae7586b4c558e284d4f
Dug Falby <dug@humansystems.io>
2023-02-06 11:12:04 +0000

    Added alternative login sequence
    Testing use of masthead page followed by gg login and html account header

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,24 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+// Handles form submissions from 'check-team-skills.html'
+router.post('/check-team-skills-routes', (req, res) => {
+
+  const haveSkills = req.body;
+  console.log("haveSkills value: ", haveSkills);
+
+  // If the checkbox is checked, redirect to agree.html
+  if (haveSkills === 'yes') {
+    res.redirect('/jasman/check-team-skills-yes.html');
+  } else {
+    // If the checkbox is not checked, redirect to not-agree.html
+    res.redirect('/jasman/check-team-skills-no.html');
+  }
+});
+
+
+
+module.exports = router;
+
