730edf2bf7341f34177a4c655dd66cdd08550406
Dug Falby <dug@humansystems.io>
2023-03-02 21:34:30 +0000

    supplier review flow added

--- app/routes.js
+++ app/routes.js
@@ -25,6 +25,22 @@ router.post('/check-team-skills-routes', (req, res) => {
 });
 
 
+// Handles form submissions from 'reviewer-team-skills.html'
+router.post('/review-team-skills-routes', (req, res) => {
+
+  const haveSkills = req.body["have-skills"];
+  console.log("haveSkills value: ", haveSkills);
+
+  // If the checkbox is checked, redirect to agree.html
+  if (haveSkills === 'yes') {
+    res.redirect('/khadija-authenticated/200-review-request.html');
+  } else {
+    // If the checkbox is not checked, redirect to not-agree.html
+    res.redirect('/khadija-authenticated/200-review-request.html');
+  }
+});
+
+
 // Handles form submissions from 'decision'
 router.post('/decision-routes', (req, res) => {
 
@@ -49,6 +65,27 @@ router.post('/decision-routes', (req, res) => {
   } 
 
 });
+
+
+// Handles form submissions from 'has team reviewed'
+router.post('/team-decision-routes', (req, res) => {
+
+  const yourDecision = req.body["team-has-reviewed"];
+  console.log("yourDecision value: ", yourDecision);
+
+  // If the checkbox is checked, redirect to agree.html
+  if (yourDecision === undefined) {
+    res.redirect('/khadija-authenticated/100-decision');
+  } 
+  if (yourDecision === 'Yes') {
+    res.redirect('/khadija-authenticated/100-decision');
+  } 
+  if (yourDecision === 'No') {
+    res.redirect('/khadija-authenticated/100-potential-risks');
+  } 
+
+});
+
 
 
 
