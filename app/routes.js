7bed26530d2f484fb1a529be2969e3266919e93b
Dug Falby <dug@humansystems.io>
2023-03-06 15:37:11 +0000

    add more steps to acquirer wizard

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,41 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 
+
+// Handles form submissions from 'What type of data do you need which is part of the acquirer wizard
+router.post('/check-for-no-need', (req, res) => {
+
+  const dataTypes = req.body["countries"];
+
+  // If none is selected exit to no agreement required
+  if (dataTypes === 'none') {
+    res.redirect('/khadija-authenticated/020-may-not-need.html');
+  } else {
+    res.redirect('/khadija-authenticated/020-lawful-basis.html');
+  }
+});
+
+
+// Handles form submissions from 'check-team-skills.html' which is part of the acquirer wizard
+router.post('/6-Mar-check-team-skills-routes', (req, res) => {
+
+  const technologySkills = req.body["technology"];
+  const securitySkills = req.body["security"];
+  const dpSkills = req.body["dp"];
+  const legalSkills = req.body["legal"];
+  const governanceSkills = req.body["governance"];
+  const businessSkills = req.body["business"];
+
+  // If one of the 'yes' radios is checked redirect to what-type-data.html
+  if (technologySkills === 'yes' || securitySkills === 'yes' || dpSkills === 'yes' || legalSkills === 'yes' || governanceSkills === 'yes' || businessSkills === 'yes') {
+    res.redirect('/khadija-authenticated/020-what-type-data.html');
+  } else {
+    // no radios checked, redirect to potential-risks.html
+    res.redirect('/khadija-authenticated/020-potential-risks.html');
+  }
+});
+
+
 // Handles form submissions from 'check-team-skills.html'
 router.post('/check-team-skills-routes', (req, res) => {
 
